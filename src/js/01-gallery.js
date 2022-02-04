388619327da136ca1e0c16906b18db38ac9cc8af
Lilia Lomaka <llomaka80@gmail.com>
2022-02-04 15:24:48 +0200

    Спроби підключення бібліотек до файлів проекту

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+// import '../../node_modules/simplelightbox/src/simple-lightbox.scss';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const listRef = document.querySelector('.gallery');
+
+function renderArrayElements(array) {
+    return array.map(({ description, original, preview }) => {
+        return  `<li><a class="gallery__item" href=${original}>
+        <img class="gallery__image" src=${preview} alt="${description}" />
+        </a></li>`;
+    }).join('');
+}
+
+const markup = renderArrayElements(galleryItems);
+listRef.insertAdjacentHTML('beforeend', markup);
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+listRef.addEventListener('click', handleImageClick);
+
+function handleImageClick(event) {
+    event.preventDefault();
+}
