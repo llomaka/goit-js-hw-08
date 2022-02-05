b59109a8ff073c286865391a5546b20b361a3c96
Lilia Lomaka <llomaka80@gmail.com>
2022-02-05 14:45:31 +0200

    Видалення зайвого слухача подій відповідно до зауваження ментора

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -16,8 +16,3 @@ function renderArrayElements(array) {
 const markup = renderArrayElements(galleryItems);
 listRef.insertAdjacentHTML('beforeend', markup);
 let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
-listRef.addEventListener('click', handleImageClick);
-
-function handleImageClick(event) {
-    event.preventDefault();
-}
