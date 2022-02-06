7f76bf803821e8d8d377277a6b817abd99e062bf
Lilia Lomaka <llomaka80@gmail.com>
2022-02-06 19:56:04 +0200

    Постійна фіксація часу відтворення відео у локальному сховищі. Відтворення із збереженого моменту

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import 'vimeo';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+const pauseVideo = function ({ seconds, percent }) {
+  if (percent < 1) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  } else {
+    localStorage.setItem('videoplayer-current-time', 0);
+  }
+};
+player.on('timeupdate', pauseVideo);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+  seconds = localStorage.getItem('videoplayer-current-time');
+}).catch(function (error) {
+  console.log(error);
+});
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
